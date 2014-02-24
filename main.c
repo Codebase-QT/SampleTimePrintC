66ab813000f6815773d3feb4576f3b22c8355052
Aromal Sasidharan <aromalsasidharan@gmail.com>
2014-02-25 00:55:59 +0530

    formatting syntax added as comment

--- main.c
+++ main.c
@@ -18,3 +18,42 @@ int main(void)
 
 }
 
+/*
+%a	Abbreviated weekday name *											   Thu
+%A	Full weekday name *												 Thursday
+%b	Abbreviated month name *												 Aug
+%B	Full month name *													 August
+%c	Date and time representation *					  Thu Aug 23 14:55:02 2001
+%d	Day of the month (01-31)												  23
+%H	Hour in 24h format (00-23)												  14
+%I	Hour in 12h format (01-12)												  02
+%j	Day of the year (001-366)												 235
+%m	Month as a decimal number (01-12)										  08
+%M	Minute (00-59)															  55
+%p	AM or PM designation													  PM
+%S	Second (00-61)															  02
+%U	Week number with the first Sunday as the first day of week one (00-53)	  33
+%w	Weekday as a decimal number with Sunday as 0 (0-6)						  4
+%W	Week number with the first Monday as the first day of week one (00-53)	  34
+%x	Date representation *												08/23/01
+%X	Time representation *												14:55:02
+%y	Year, last two digits (00-99)											  01
+%Y	Year																	2001
+%Z	Timezone name or abbreviation											 CDT
+%%	A % sign
+
+    * The above specifiers whose description is marked
+      with an asterisk (*) are locale-dependent.
+
+    See ... (ref.)
+    http://www.cplusplus.com/reference/clibrary/ctime/strftime.html
+
+    Or ... (me)
+    http://www.geocities.com/dwzavitz/
+    http://securejoules.googlepages.com/
+    http://developers-heaven.net/forum/index.php/topic,46.0.html
+
+*/
+
+
+
